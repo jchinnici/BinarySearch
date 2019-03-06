48f1ae9330d3a3dce89f1869eb4047d16164814f
jchinnici <joseph.t.chinnici@gmail.com>
2019-03-06 18:28:30 -0500

    writing more code

---
+++ ConsoleApp5/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class InsertionSort
+    {
+        static int[] array = new int[] { 2, 1, 4, 3, 5, 0, 7, 6, 8, 9 };
+        public InsertionSort()
+        {
+
+            Insert_sort();
+
+        }
+        public static void Insert_sort()
+        {
+            int tmp;
+            
+            //O^n 
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int l = 0; l < array.Length; l++)
+                {
+                    if (array[i] < array[l])
+                    {
+                        tmp = array[l];
+                        array[l] = array[i];
+                        array[i] = tmp;
+                    }
+                }
+            }
+            foreach (var item in array)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.Read();
+        }
+    }
+ 
+
+
+
+
+}
--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -8,8 +8,38 @@ namespace ConsoleApp5
 {
     class Program
     {
+        static int[] array = new int[] { 2, 1, 4, 3, 5, 0, 7, 6, 8, 9 };
         static void Main(string[] args)
         {
+            
+            new InsertionSort();
+            public static object BinaryS(int search)
+            {
+                Console.Write("Enter number to find ");
+                search = Int32.Parse(Console.ReadLine());
+                int min = 0;
+                int max = array.Length - 1;
+                int key = 0;
+                while (min <= max)
+                {
+                    int mid = (min + max) / 2;
+                    if (search == array[mid])
+                    {
+                        return mid;
+                    }
+                    else if (search < array[mid])
+                    {
+                        max = mid - 1;
+                    }
+
+                }
+            }
+
+
+            
+
+
         }
+        
     }
 }
