4f1361d04ea95ef0b1b2644cde0b59ac858fc09a
jchinnici <joseph.t.chinnici@gmail.com>
2019-03-08 18:20:08 -0500

    made changes

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -12,36 +12,38 @@ namespace ConsoleApp5
 
         static void Main(string[] args)
         {
-            
-            
+
+
             Console.WriteLine("Enter number to find ");
             int search = Int32.Parse(Console.ReadLine());
-            object BinaryS(int[] array, int key)
-            {
-                int min = 0;
-                int max = array.Length - 1;
-                while (min <= max)
-                {
-                    int mid = (min + max) / 2;
-                    if (key == array[mid])
-                    {
-                        return mid;
-                    }
-                    else if (key < array[mid])
-                    {
-                        max = mid - 1;
-                    }
-                    else if (key > array[mid])
-                    {
-                        min = mid + 1;
-                    }
 
-                }
-                return "Int not found";
 
-            }
+
             array.Insert_sort();
             Console.Read();
+        }
+        object BinaryS(int[] array, int key)
+        {
+            int min = 0;
+            int max = array.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (key == array[mid])
+                {
+                    return mid;
+                }
+                else if (key < array[mid])
+                {
+                    max = mid - 1;
+                }
+                else if (key > array[mid])
+                {
+                    min = mid + 1;
+                }
+
+            }
+            return "Int not found";
         }
     }
 }
